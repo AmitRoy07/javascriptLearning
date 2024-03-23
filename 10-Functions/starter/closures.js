1b8fa581de41c0253df6a2de5045f520758c3606
Amit Roy <amitroy2383@gmail.com>
2024-03-23 20:18:49 +0530

    closures section done

--- 10-Functions/starter/closures.js
+++ 10-Functions/starter/closures.js
@@ -13,3 +13,44 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+
+// example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// re-assign the f function
+h();
+f();
+
+console.dir(f);
+
+// example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(() => {
+    console.log(`we are now boardPassengers ${n}`);
+    console.log(`there are 3 group, each with ${perGroup} passenger`);
+  }, wait * 1000);
+
+  console.log(`will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
